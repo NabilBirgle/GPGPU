185d25fd5edc22bc8be70a7bc997c8063215d047
Nabil BIRGLE <nabil.birgle@gmail.com>
2024-05-18 11:54:35 +0200

    old version from CPU

--- GPGPU/GPU.swift
+++ GPGPU/GPU.swift
@@ -1,200 +1,78 @@
 import MetalKit
 
 class GPU {
-	private var device: MTLDevice?
-	private var library: MTLLibrary?
-	init(){
+	let device: MTLDevice?
+	let commandQueue: MTLCommandQueue?
+	let library: MTLLibrary?
+	init() {
 		self.device = MTLCreateSystemDefaultDevice()
+		self.commandQueue = device?.makeCommandQueue()
 		self.library = device?.makeDefaultLibrary()
 	}
-	func get_device() -> MTLDevice? {
-		device
-	}
-	private var functions: [String: MTLFunction] = [:]
+	var functions: [String: MTLFunction] = [:]
 	func compile(name: String){
 		functions[name] = library?.makeFunction(name: name)
 	}
-//	private var render_pipeline_state: MTLRenderPipelineState?
-//	func set_render_pipeline_state(metalView: MTKView,
-//								   vertex_function: String,
-//								   fragment_function: String,
-//								   n_buffer: Int){
-//		let pipelineDescriptor: MTLRenderPipelineDescriptor = MTLRenderPipelineDescriptor()
-//		pipelineDescriptor.vertexFunction = functions[vertex_function]
-//		pipelineDescriptor.fragmentFunction = functions[fragment_function]
-//		pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
-//		pipelineDescriptor.vertexDescriptor =
-//		MTLVertexDescriptor.defaultLayout(n_buffer: n_buffer)
-//		do {
-//			render_pipeline_state =
-//			try device?.makeRenderPipelineState(descriptor: pipelineDescriptor)
-//		} catch {
-//			fatalError(error.localizedDescription)
-//		}
-//	}
-//	func get_render_pipeline_state() -> MTLRenderPipelineState? {
-//		render_pipeline_state
-//	}
-	private var compute_pipeline_state: MTLComputePipelineState?
-	func set_compute_pipeline_state(function_name: String){
+	var pipeline_state: MTLComputePipelineState?
+	func set_gpgpu_pipeline(function_name: String){
 		guard
 			let f: MTLFunction = functions[function_name]
 		else {
-			compute_pipeline_state = nil
 			return
 		}
 		do {
-			compute_pipeline_state = try device?.makeComputePipelineState(function: f)
+			pipeline_state = try device?.makeComputePipelineState(function: f)
 		} catch {
 			print(error)
 		}
 	}
-	func get_compute_pipeline_state() -> MTLComputePipelineState? {
-		compute_pipeline_state
-	}
 }
 
-class Command_queue {
-	private var command_queue: MTLCommandQueue?
-	init(gpu: GPU) {
-		command_queue = gpu.get_device()?.makeCommandQueue()
-	}
-	func make_command_buffer() -> MTLCommandBuffer? {
-		command_queue?.makeCommandBuffer()
-	}
-}
-
-class Command_buffer {
-	private var command_buffer: MTLCommandBuffer?
-	init(command_queue: Command_queue){
-		self.command_buffer = command_queue.make_command_buffer()
-	}
-	func present(view: MTKView){
-		guard
-			let drawable = view.currentDrawable
-		else {
-			command_buffer = nil
-			return
+extension GPU {
+	class GPGPU{
+		let gpu: GPU
+		init(gpu: GPU) {
+			self.gpu = gpu
 		}
-		command_buffer?.present(drawable)
-	}
-	func make_render_command_encoder(descriptor: MTLRenderPassDescriptor) -> MTLRenderCommandEncoder? {
-		command_buffer?.makeRenderCommandEncoder(descriptor: descriptor)
-	}
-	func make_compute_command_encoder() -> MTLComputeCommandEncoder? {
-		command_buffer?.makeComputeCommandEncoder()
-	}
-	func commit(){
-		command_buffer?.commit()
-		command_buffer?.waitUntilCompleted()
-	}
-}
-
-//class Render_command_encoder{
-//	private var command_encoder: MTLRenderCommandEncoder?
-//	init(view: MTKView, command_buffer: Command_buffer) {
-//		guard
-//			let descriptor: MTLRenderPassDescriptor = view.currentRenderPassDescriptor
-//		else {
-//			command_encoder = nil
-//			return
-//		}
-//		command_encoder = command_buffer.make_render_command_encoder(
-//			descriptor: descriptor
-//		)
-//	}
-//	var input: Int = 0
-//	func set_input(x: inout Float){
-//		command_encoder?.setVertexBytes(
-//			&x, length: MemoryLayout<Float>.stride, index: input
-//		)
-//		input += 1
-//	}
-//	func set_input(x: inout simd_float2){
-//		command_encoder?.setVertexBytes(
-//			&x, length: MemoryLayout<simd_float2>.stride, index: input
-//		)
-//		input += 1
-//	}
-//	func set_input(arr: MTLBuffer){
-//		command_encoder?.setVertexBuffer(arr, offset: 0, index: input)
-//		input += 1
-//	}
-//	func set_shader_input(window: Window, 
-//						  render_pipeline_state: MTLRenderPipelineState?){
-//		set_input(arr: window.mesh.vertex_buffer)
-//		set_input(arr: window.mesh.color_buffer)
-//		guard
-//			let pipeline_state: MTLRenderPipelineState = render_pipeline_state
-//		else {
-//			command_encoder = nil
-//			return
-//		}
-//		command_encoder?.setRenderPipelineState(pipeline_state)
-//		command_encoder?.drawIndexedPrimitives(
-//			type: .triangle,
-//			indexCount: window.mesh.n_t,
-//			indexType: .uint32,
-//			indexBuffer: window.mesh.triangles_buffer,
-//			indexBufferOffset: 0)
-//	}
-//	func end(){
-//		command_encoder?.endEncoding()
-//	}
-//}
-
-class Compute_command_encoder{
-	private var command_encoder: MTLComputeCommandEncoder?
-	init(command_buffer: Command_buffer){
-		command_encoder = command_buffer.make_compute_command_encoder()
-	}
-	var input: Int = 0
-	func set_input(x: inout Bool){
-		command_encoder?.setBytes(&x,
-								  length: MemoryLayout<Bool>.stride,
-								  index: input)
-		input += 1
-	}
-	func set_input(x: inout UInt32){
-		command_encoder?.setBytes(&x,
-								  length: MemoryLayout<UInt32>.stride,
-								  index: input)
-		input += 1
-	}
-	func set_input(x: inout Float){
-		command_encoder?.setBytes(&x,
-								  length: MemoryLayout<Float>.stride,
-								  index: input)
-		input += 1
-	}
-	func set_input(x: inout simd_float2){
-		command_encoder?.setBytes(&x,
-								  length: MemoryLayout<simd_float2>.stride,
-								  index: input)
-		input += 1
-	}
-	func set_input(arr: MTLBuffer){
-		command_encoder?.setBuffer(arr, offset: 0, index: input)
-		input += 1
-	}
-//	func set_input(GPU: gpu, arr: [Float], n: Int){
-//		   command_encoder?.setBuffer(buffer, offset: 0, index: input)
-//		   input += 1
-//	   }
-	func set_index_input(thread: Int, compute_pipeline_state: MTLComputePipelineState?){
-		guard
-			let pipeline_state: MTLComputePipelineState = compute_pipeline_state
-		else {
-			command_encoder = nil
-			return
+		var command_buffer: MTLCommandBuffer?
+		var command_encoder: MTLComputeCommandEncoder?
+		func call_kernel_function(){
+			command_buffer = gpu.commandQueue?.makeCommandBuffer()
+			command_encoder = command_buffer?.makeComputeCommandEncoder()
+			command_encoder?.setComputePipelineState(gpu.pipeline_state!)
 		}
-		command_encoder?.setComputePipelineState(pipeline_state)
-		let grid: MTLSize = MTLSize(width: thread, height: 1, depth: 1)
-		let k: Int = pipeline_state.maxTotalThreadsPerThreadgroup
-		let subgrid: MTLSize = MTLSize(width: k, height: 1, depth: 1)
-		command_encoder?.dispatchThreads(grid, threadsPerThreadgroup: subgrid)
-	}
-	func end(){
-		command_encoder?.endEncoding()
+		var input = 0
+		func set_input(arr: [Float], n: Int){
+			let buffer = gpu.device?.makeBuffer(bytes: arr,
+												length: MemoryLayout<Float>.size * n,
+												options: .storageModeShared)
+			command_encoder?.setBuffer(buffer, offset: 0, index: input)
+			input += 1
+		}
+		func set_input(arr: UnsafeMutablePointer<Float>, n: Int){
+			let buffer = gpu.device?.makeBuffer(bytes: arr,
+												length: MemoryLayout<Float>.size * n,
+												options: .storageModeShared)
+			command_encoder?.setBuffer(buffer, offset: 0, index: input)
+			input += 1
+		}
+		func set_output(n: Int) -> UnsafeMutablePointer<Float> {
+			let buffer = gpu.device?.makeBuffer(length: MemoryLayout<Float>.size * n,
+												options: .storageModeShared)
+			command_encoder?.setBuffer(buffer, offset: 0, index: input)
+			input += 1
+			return (buffer?.contents().bindMemory(
+				to: Float.self, capacity: MemoryLayout<Float>.size * n))!
+		}
+		func set_index_input(thread: Int){
+			let grid: MTLSize = MTLSize(width: thread, height: 1, depth: 1)
+			let k: Int = gpu.pipeline_state!.maxTotalThreadsPerThreadgroup
+			let subgrid: MTLSize = MTLSize(width: k, height: 1, depth: 1)
+			command_encoder?.dispatchThreads(grid, threadsPerThreadgroup: subgrid)
+			command_encoder?.endEncoding()
+			command_buffer?.commit()
+			command_buffer?.waitUntilCompleted()
+			input = 0
+		}
 	}
 }
--- GPGPU/main.swift
+++ GPGPU/main.swift
@@ -15,7 +15,7 @@ func computeWay(arr1: [Float], arr2: [Float]) {
 	let startTime = CFAbsoluteTimeGetCurrent()
 
 	let gpu: GPU = GPU()
-	let command_queue = Command_queue(gpu: gpu)
+	let gpgpu: GPU.GPGPU = GPU.GPGPU(gpu: gpu)
 
 	let f = "addition_compute_function"
 	let g = "reduce_function"
@@ -25,55 +25,25 @@ func computeWay(arr1: [Float], arr2: [Float]) {
 	print()
 	print("GPU Way")
 
-	var command_buffer: Command_buffer
-	var command_encoder: Compute_command_encoder
+	gpu.set_gpgpu_pipeline(function_name: f)
+	gpgpu.call_kernel_function()
+	gpgpu.set_input(arr: arr1, n: count)
+	gpgpu.set_input(arr: arr2, n: count)
+	let arr3 = gpgpu.set_output(n: count)
+	gpgpu.set_index_input(thread: count)
 
-	guard 
-		let A1: MTLBuffer = gpu.get_device()?.makeBuffer(bytes: arr1,
-											  length: MemoryLayout<Float>.size * count,
-											  options: .storageModeShared),
-		let A2: MTLBuffer = gpu.get_device()?.makeBuffer(bytes: arr2,
-											  length: MemoryLayout<Float>.size * count,
-											  options: .storageModeShared),
-		let A3: MTLBuffer = gpu.get_device()?.makeBuffer(
-											  length: MemoryLayout<Float>.size * count,
-											  options: .storageModeShared),
-		let A4: MTLBuffer = gpu.get_device()?.makeBuffer(
-											  length: MemoryLayout<Float>.size,
-											  options: .storageModeShared)
-	else {
-		return
-	}
+	gpu.set_gpgpu_pipeline(function_name: g)
+	gpgpu.call_kernel_function()
+	gpgpu.set_input(arr: arr3, n: count)
+	let arr4 = gpgpu.set_output(n: 1)
+	gpgpu.set_index_input(thread: count)
 
-	command_buffer = Command_buffer(command_queue: command_queue)
-	command_encoder = Compute_command_encoder(command_buffer: command_buffer)
-	gpu.set_compute_pipeline_state(function_name: f)
-	command_encoder.set_input(arr: A1)
-	command_encoder.set_input(arr: A2)
-	command_encoder.set_input(arr: A3)
-	command_encoder.set_index_input(thread: count, compute_pipeline_state: gpu.get_compute_pipeline_state())
-	command_encoder.end()
-	command_buffer.commit()
-
-	command_buffer = Command_buffer(command_queue: command_queue)
-	command_encoder = Compute_command_encoder(command_buffer: command_buffer)
-	gpu.set_compute_pipeline_state(function_name: g)
-	command_encoder.set_input(arr: A3)
-	command_encoder.set_input(arr: A4)
-	command_encoder.set_index_input(thread: count, compute_pipeline_state: gpu.get_compute_pipeline_state())
-	command_encoder.end()
-	command_buffer.commit()
-
-	let B3 = (A3.contents().bindMemory(
-		to: Float.self, capacity: MemoryLayout<Float>.size * count))
-	let B4 = (A4.contents().bindMemory(
-		to: Float.self, capacity: MemoryLayout<Float>.size))
 
 	// Print out all of our new added together array information
 	for i in 0..<3 {
-		print("\(arr1[i]) + \(arr2[i]) = \(B3[i])")
+		print("\(arr1[i]) + \(arr2[i]) = \(arr3[i])")
 	}
-	print("Sum(arr3) = \(B4[0])")
+	print("Sum(arr3) = \(arr4[0])")
 
 	// Print out the elapsed time
 	let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
